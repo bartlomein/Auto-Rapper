da0a77ca79b33afcf564cd3ab3e4eabcc04fb08b
bartlomein <bartlomein123@gmail.com>
2018-01-25 06:19:14 -0500

    back to nltk

--- rhyme1.py
+++ rhyme1.py
@@ -1,7 +1,8 @@
+
+import nltk
 import pyphen
 from random import randint
 import markovify
-import pronouncing
 
 with open("rap.txt") as f:
     text = f.read()
@@ -10,6 +11,13 @@ dic = pyphen.Pyphen(lang='en')
 
 text_model = markovify.Text(text)
 
+def rhyme(inp, level):
+    entries = nltk.corpus.cmudict.entries()
+    syllables = [(word, syl) for word, syl in entries if word == inp]
+    rhymes = []
+    for (word, syllable) in syllables:
+        rhymes += [word for word, pron in entries if pron[-level:] == syllable[-level:]]
+    return set(rhymes)
 
 def generate_model(cfdist, word, num=15):
     for i in range(num):
@@ -27,7 +35,7 @@ def replace_last_word(first, rhyme_word):
     return new
 
 def select_rhyme_word(word1):
-    word_that_rhyme = pronouncing.rhymes(word1)
+    word_that_rhyme = rhyme(word1, 2)
     wordlist = list(word_that_rhyme)
 
     if len(wordlist) == 0:
@@ -51,7 +59,6 @@ second = text_model.make_short_sentence(100)
 third = text_model.make_short_sentence(100)
 fourth = text_model.make_short_sentence(100)
 print(sentence)
-
 print(replace_last_word(first, select_rhyme_word(last_word1)))
 print(replace_last_word(second, select_rhyme_word(last_word1)))
 print(replace_last_word(third, select_rhyme_word(last_word1)))
